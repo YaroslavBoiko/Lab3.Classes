72d482aff813bf2c59f2b65014aec9a9749c9c08
Yaroslav <35149052+YaroslavBoiko@users.noreply.github.com>
2018-11-13 13:49:43 +0200

    Update City.java

--- src/main/java/City.java
+++ src/main/java/City.java
@@ -21,6 +21,28 @@ public class City {
 //    private String area;
 
     private static final int INFO_SIZE = 6;
+    
+    public void printInfo() {
+        System.out.println("City name: " + this.getName());
+        System.out.println("City administrativeArea: " + this.getAdministrativeArea());
+        System.out.println("City number Of Citizens: " + this.getNumberOfCitizens());
+        System.out.println("City coordinates: " + this.getCoordinates().lat + " " + this.getCoordinates().lon);
+        System.out.println("City area: " + this.getArea());
+    }
+    
+    
+    public static int parsePopulation(Element element) {
+        
+        int element_size = element.getAllElements().size();
+        
+        if (element_size > 1) {
+                element = element.getAllElements().get(element_size - 1);
+            }
+            String _anchor2 = element.text().replaceAll("(&nbsp;|\\s)","").replace("\u00a0","");
+        
+            return Integer.parseInt(_anchor2);
+    
+    }
 
 
     public static City parse(Element city) {
@@ -32,13 +54,15 @@ public class City {
             Element anchor2 = info.get(3);
             Element anchor3 = info.get(4).select("a").get(0);
             Element anchor4 = info.get(5);
+       
 
             City myCity = new City();
+            
 
             myCity.setName(anchor.attr("title"));
             myCity.setUrl(String.format("https://uk.wikipedia.org%s", anchor.attr("href")));
             myCity.setAdministrativeArea(anchor1.attr("title"));
-            myCity.setNumberOfCitizens(Integer.parseInt(anchor2.text().replaceAll("\\s+","")));
+            myCity.setNumberOfCitizens(parsePopulation(anchor2));
             myCity.setYearOfFound(anchor3.attr("title"));
             myCity.setArea(Double.valueOf(anchor4.text()));
 
@@ -46,11 +70,17 @@ public class City {
             Coordinates coordinates = new Coordinates();
             String url = myCity.url;
             double[] s = coordinates.Coordinat(url);
-            coordinates.setlan(s[0]);
-            coordinates.setlon(s[1]);
-            myCity.setCoordinates(coordinates);
+            
+            if (s != null) {
+                coordinates.setlan(s[0]);
+                coordinates.setlon(s[1]);
+                myCity.setCoordinates(coordinates);
 
-            return myCity;
+                return myCity;
+            } else {
+                System.out.println("Sorry, we didn`t support this city!");
+                return null;
+            }
         }
         return null;
     }
